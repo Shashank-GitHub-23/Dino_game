e6809e336bd8b4cfff25184d9a1893a6e48a6053
Shashank Shukla <64839400+Shashank-GitHub-23@users.noreply.github.com>
2023-09-01 23:22:04 +0530

    Almost Completed

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-var dino = document.getElementById("character");
-var block = document.getElementById("block");
-var result = document.getElementById("result");
-var score = document.getElementById("score");
-var game = document.getElementById("game");
-var count= 0;
+let dino = document.getElementById("character");
+let block = document.getElementById("block");
+let result = document.getElementById("result");
+let score = document.getElementById("score");
+let game = document.getElementById("game");
+let count= 0;
 
 function jump(){
     dino.style.top = "290px";
     setTimeout(function (){
      dino.style.top = "376px";
-    },500)
+    },400)
     count++;
 }
 window.addEventListener('keydown' , jump);
 window.addEventListener('click', jump);
 
 setInterval(function gameOver(){
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     if((blockLeft < 52) && (dinoTop > 302) ){
     result.style.display = "block";
-    block.style.animationPlayState = "paused"; 
-    // game.style.display = "none";
+    block.style.animationPlayState = "paused";
     score.innerHTML = `Score: ${count}`;
     }
 },10);
